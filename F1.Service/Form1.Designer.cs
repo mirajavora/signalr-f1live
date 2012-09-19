65f9a0b69e40e2683a0b0450dea3707d212cf35c
Mira Javora <mira@stormid.com>
2012-09-19 23:10:25 +0100

    Added the win forms project

---
+++ F1.Service/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace F1.Service
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EndPointUrl = new System.Windows.Forms.TextBox();
+            this.ReloadButton = new System.Windows.Forms.Button();
+            this.ListData = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // EndPointUrl
+            // 
+            this.EndPointUrl.Location = new System.Drawing.Point(12, 12);
+            this.EndPointUrl.Name = "EndPointUrl";
+            this.EndPointUrl.Size = new System.Drawing.Size(312, 20);
+            this.EndPointUrl.TabIndex = 0;
+            this.EndPointUrl.Text = "http://localhost:49232/";
+            // 
+            // ReloadButton
+            // 
+            this.ReloadButton.Location = new System.Drawing.Point(330, 10);
+            this.ReloadButton.Name = "ReloadButton";
+            this.ReloadButton.Size = new System.Drawing.Size(163, 23);
+            this.ReloadButton.TabIndex = 1;
+            this.ReloadButton.Text = "ReLoad";
+            this.ReloadButton.UseVisualStyleBackColor = true;
+            this.ReloadButton.Click += new System.EventHandler(this.ReloadButton_Click);
+            // 
+            // ListData
+            // 
+            this.ListData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.ListData.Location = new System.Drawing.Point(12, 38);
+            this.ListData.Name = "ListData";
+            this.ListData.Size = new System.Drawing.Size(712, 323);
+            this.ListData.TabIndex = 2;
+            this.ListData.UseCompatibleStateImageBehavior = false;
+            this.ListData.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Tag = "";
+            this.columnHeader1.Text = "Action";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 373);
+            this.Controls.Add(this.ListData);
+            this.Controls.Add(this.ReloadButton);
+            this.Controls.Add(this.EndPointUrl);
+            this.Name = "Form1";
+            this.Text = "F1 Service";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox EndPointUrl;
+        private System.Windows.Forms.Button ReloadButton;
+        private System.Windows.Forms.ListView ListData;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ F1.Service/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using SignalR.Client.Hubs;
+
+namespace F1.Service
+{
+    public partial class Form1 : Form
+    {
+        private HubConnection _connection;
+        private IList<dynamic> _leaderboard;
+        private IList<string> _photos;
+        private int _photoIndex;
+        private IHubProxy _leaderboardHub;
+        private IHubProxy _imageHub;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            AddItem("Service started", "OK");
+            var instagramTimer = new Timer();
+            var leaderboardTimer = new Timer();
+
+            //create proxy
+            _connection = new HubConnection(EndPointUrl.Text);
+            _leaderboardHub = _connection.CreateProxy("LeaderboardHub");
+            _imageHub = _connection.CreateProxy("ImagesHub");
+            _connection.Start();
+            
+            AddItem("Connection State", _connection.State.ToString());
+
+            //init leaderboard
+            _leaderboard = CreateLeaderboard();
+            _photos = GetPhotos();
+            _photoIndex = 0;
+
+            //init timers
+            instagramTimer.Interval = 10000;
+            instagramTimer.Tick +=  InstagramElapsed;
+            leaderboardTimer.Interval = 5000;
+            leaderboardTimer.Tick += LeaderboardElapsed;
+
+            instagramTimer.Start();
+            leaderboardTimer.Start();
+        }
+
+        private IList<dynamic> CreateLeaderboard()
+        {
+            var leaders = new List<dynamic>();
+            var names = new List<string>()
+                            {
+                                "Lewis Hamilton",
+                                "Jensen Button",
+                                "Fernando Alonso",
+                                "Felipe Massa",
+                                "Sebastien Vettel",
+                                "Michael Schumacher",
+                                "Nico Roseberg",
+                                
+                            };
+            for (var i = 1; i < 7; i++)
+            {
+                leaders.Add(CreateLeaderboardItem(names[i-1], i));
+            }
+
+            return leaders;
+        }
+
+        private object CreateLeaderboardItem(string name, int position)
+        {
+            return new {Name = name, Position = position, PositionChange = 0};
+
+        }
+
+        void LeaderboardElapsed(object sender, EventArgs e)
+        {
+            AddItem("Connection State", _connection.State.ToString());
+
+            _leaderboardHub.Invoke("PublishLeaderboard", GetLeaderboard()).ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    AddItem("Leaderboard", string.Format("An error occurred during the method call {0}", task.Exception.GetBaseException()));
+                }
+                else
+                {
+                    AddItem("Leaderboard", "Successfully called PublishLeaderboard");
+
+                }
+            });
+        }
+
+        private void InstagramElapsed(object sender, EventArgs e)
+        {
+            if (_photoIndex >= _photos.Count)
+                _photoIndex = 0;
+
+            var image = _photos[_photoIndex];
+
+            _imageHub.Invoke("PublishPhoto", image).ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    AddItem("Instagram", string.Format("An error occurred during the method call {0}", task.Exception.GetBaseException()));
+                }
+                else
+                {
+                    AddItem("Instagram", image);
+
+                }
+            });
+            _photoIndex++;
+        }
+
+        private void ReloadButton_Click(object sender, EventArgs e)
+        {
+            //create proxy
+            _connection = new HubConnection(EndPointUrl.Text);
+            _connection.Start();
+        }
+
+        private void AddItem(string action, string result)
+        {
+            var item = new ListViewItem(new[] {action, DateTime.Now.ToString(), result});
+            ListData.Items.Add(item);
+        }
+
+        private IList<dynamic> GetLeaderboard()
+        {
+            //clear position change property
+            for (int index = 0; index < _leaderboard.Count; index++)
+            {
+                var o = _leaderboard[index];
+                _leaderboard[index] = new {Name = o.Name, Position = o.Position, PositionChange = 0};
+            }
+
+            //swap two places
+            var total = _leaderboard.Count;
+            var rnd = new Random();
+            var swapIndex = rnd.Next(0, total - 2);
+
+            var original = _leaderboard[swapIndex];
+            var next = _leaderboard[swapIndex + 1];
+
+            _leaderboard[swapIndex + 1] = new { Name = original.Name, Position = original.Position, PositionChange = 1 }; ;
+            _leaderboard[swapIndex] = new { Name = next.Name, Position = next.Position, PositionChange = -1 }; ; ;
+
+            return _leaderboard;
+        }
+
+
+        private IList<string> GetPhotos()
+        {
+            var photos = new List<string>();
+            try
+            {
+                var config = new InstaSharp.InstagramConfig("https://api.instagram.com/v1", "https://api.instagram.com/oauth", "ec801d2b6eff42aaa5dc1d18bfc5e74b", "9c2aa521f5df4019b298a816bcfd4a4c", "http://localhost:56137/");
+                var tagSearch = new InstaSharp.Endpoints.Tags.Unauthenticated(config);
+                var searchResults = tagSearch.Recent(HttpUtility.UrlEncode("f1"));
+
+                photos.AddRange(searchResults.Data.Select(x => x.Images.LowResolution.Url).ToList());
+
+                return photos;
+            }
+            catch (Exception ex)
+            {
+                //load the fake phoots
+                photos.Add("http://google.com/image.jpg");
+            }
+            return photos;
+        }
+    }
+}
